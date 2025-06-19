730b3087291545cebc6ae94d050ceddf96fec156
PlingifyPlug ("Vinti") <168746576+BackupPASS@users.noreply.github.com>
2025-06-19 01:05:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ function createNotificationCard(platform, title, desc, downloadLink, requirement
     icon.className = 'icon';
     icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <g id="SVGRepo_iconCarrier">
-         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#ffffff" d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"></path>
+         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#f222" d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"></path>
     </g>
   </svg>`;
 
@@ -162,7 +162,7 @@ function createNotificationCard(platform, title, desc, downloadLink, requirement
     const closeButton = document.createElement('button');
     closeButton.type = 'button';
     closeButton.className = 'close';
-    closeButton.innerHTML = `<svg aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+   closeButton.innerHTML = `<svg aria-hidden="true" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
         <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
      </svg>`;
 
